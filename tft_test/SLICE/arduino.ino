93709d8f8deea249b4d607dd198237f60fdcb6e2
Thomas Desrosiers <thomas.desrosiers@live.fr>
2022-02-06 14:59:01 -0500

    + Ajout de text affichant la valeur actuelle qui est affiché sur l,élément graphique. ~ Les valeur sont changés en fonction de la valeur reçu par le terminal série.

--- tft_test/SLICE/arduino.ino
+++ tft_test/SLICE/arduino.ino
@@ -14,13 +14,14 @@
 // ------------------------------------------------
 #include "arduino_GSLC.h"
 
-
 // ------------------------------------------------
 // Program Globals
 // ------------------------------------------------
 
 // Save some element references for direct access
 //<Save_References !Start!>
+gslc_tsElemRef* m_pElemCounter1   = NULL;
+gslc_tsElemRef* m_pElemCounter2   = NULL;
 gslc_tsElemRef* m_pElemRadial1    = NULL;
 gslc_tsElemRef* m_pElemRamp2_3    = NULL;
 gslc_tsElemRef* m_pElemToggle1    = NULL;
@@ -28,50 +29,60 @@ gslc_tsElemRef* m_pElemXRingGauge1= NULL;
 //<Save_References !End!>
 
 // Define debug message function
-static int16_t DebugOut(char ch) { if (ch == (char)'\n') Serial.println(""); else Serial.write(ch); return 0; }
+static int16_t DebugOut(char ch)
+{
+  if (ch == (char)'\n')
+    Serial.println("");
+  else
+    Serial.write(ch);
+  return 0;
+}
 
 // ------------------------------------------------
 // Callback Methods
 // ------------------------------------------------
 // Common Button callback
-bool CbBtnCommon(void* pvGui,void *pvElemRef,gslc_teTouch eTouch,int16_t nX,int16_t nY)
+bool CbBtnCommon(void *pvGui, void *pvElemRef, gslc_teTouch eTouch, int16_t nX, int16_t nY)
 {
   // Typecast the parameters to match the GUI and element types
-  gslc_tsGui*     pGui     = (gslc_tsGui*)(pvGui);
-  gslc_tsElemRef* pElemRef = (gslc_tsElemRef*)(pvElemRef);
-  gslc_tsElem*    pElem    = gslc_GetElemFromRef(pGui,pElemRef);
+  gslc_tsGui *pGui = (gslc_tsGui *)(pvGui);
+  gslc_tsElemRef *pElemRef = (gslc_tsElemRef *)(pvElemRef);
+  gslc_tsElem *pElem = gslc_GetElemFromRef(pGui, pElemRef);
 
-  if ( eTouch == GSLC_TOUCH_UP_IN ) {
+  if (eTouch == GSLC_TOUCH_UP_IN)
+  {
     // From the element's ID we can determine which button was pressed.
-    switch (pElem->nId) {
+    switch (pElem->nId)
+    {
 //<Button Enums !Start!>
-      case E_ELEM_TOGGLE1:
-        // TODO Add code for Toggle button ON/OFF state
-        if (gslc_ElemXTogglebtnGetState(&m_gui, m_pElemToggle1)) {
-          ;
-        }
-        break;
-      case E_ELEM_BTN9:
-        gslc_SetPageCur(&m_gui, E_PG2);
-        break;
-      case E_ELEM_BTN10:
-        gslc_SetPageCur(&m_gui, E_PG3);
-        break;
-      case E_ELEM_BTN7:
-        gslc_SetPageCur(&m_gui, E_PG1);
-        break;
-      case E_ELEM_BTN8:
-        gslc_SetPageCur(&m_gui, E_PG3);
-        break;
-      case E_ELEM_BTN5:
-        gslc_SetPageCur(&m_gui, E_PG1);
-        break;
-      case E_ELEM_BTN6:
-        gslc_SetPageCur(&m_gui, E_PG2);
-        break;
+    case E_ELEM_BTN9:
+      gslc_SetPageCur(&m_gui, E_PG2);
+      break;
+    case E_ELEM_BTN10:
+      gslc_SetPageCur(&m_gui, E_PG3);
+      break;
+    case E_ELEM_TOGGLE1:
+      // TODO Add code for Toggle button ON/OFF state
+      if (gslc_ElemXTogglebtnGetState(&m_gui, m_pElemToggle1))
+      {
+        ;
+      }
+      break;
+    case E_ELEM_BTN7:
+      gslc_SetPageCur(&m_gui, E_PG1);
+      break;
+    case E_ELEM_BTN8:
+      gslc_SetPageCur(&m_gui, E_PG3);
+      break;
+    case E_ELEM_BTN5:
+      gslc_SetPageCur(&m_gui, E_PG1);
+      break;
+    case E_ELEM_BTN6:
+      gslc_SetPageCur(&m_gui, E_PG2);
+      break;
 //<Button Enums !End!>
-      default:
-        break;
+    default:
+      break;
     }
   }
   return true;
@@ -80,25 +91,8 @@ bool CbBtnCommon(void* pvGui,void *pvElemRef,gslc_teTouch eTouch,int16_t nX,int1
 //<Checkbox Callback !End!>
 //<Keypad Callback !Start!>
 //<Keypad Callback !End!>
-// Spinner Input Ready callback
-bool CbSpinner(void* pvGui, void *pvElemRef, int16_t nState, void* pvData)
-{
-  gslc_tsGui*     pGui = (gslc_tsGui*)pvGui;
-  gslc_tsElemRef* pElemRef = (gslc_tsElemRef*)(pvElemRef);
-  gslc_tsElem*    pElem = gslc_GetElemFromRef(pGui,pElemRef);
-
-  // NOTE: pvData is NULL
-  if (nState == XSPINNER_CB_STATE_UPDATE) {
-    // From the element's ID we can determine which input field is ready.
-    switch (pElem->nId) {
-//<Spinner Enums !Start!>
-
-//<Spinner Enums !End!>
-      default:
-        break;
-    }
-  }
-}
+//<Spinner Callback !Start!>
+//<Spinner Callback !End!>
 //<Listbox Callback !Start!>
 //<Listbox Callback !End!>
 //<Draw Callback !Start!>
@@ -114,8 +108,8 @@ void setup()
   // Initialize
   // ------------------------------------------------
   Serial.begin(9600);
-  // Wait for USB Serial 
-  //delay(1000);  // NOTE: Some devices require a delay after Serial.begin() before serial port can be used
+  // Wait for USB Serial
+  // delay(1000);  // NOTE: Some devices require a delay after Serial.begin() before serial port can be used
 
   gslc_InitDebug(&DebugOut);
 
@@ -123,7 +117,6 @@ void setup()
   // Create graphic elements
   // ------------------------------------------------
   InitGUIslice_gen();
-
 }
 
 // -----------------------------------
@@ -135,13 +128,11 @@ void loop()
   // ------------------------------------------------
   // Update GUI Elements
   // ------------------------------------------------
-  
-  //TODO - Add update code for any text, gauges, or sliders
-  
+
+  // TODO - Add update code for any text, gauges, or sliders
+
   // ------------------------------------------------
   // Periodically call GUIslice update function
   // ------------------------------------------------
   gslc_Update(&m_gui);
-    
 }
-
--- tft_test/SLICE/arduino_GSLC.h
+++ tft_test/SLICE/arduino_GSLC.h
@@ -21,13 +21,10 @@
 // Include any extended elements
 //<Includes !Start!>
 // Include extended elements
-#include "elem/XKeyPad_Num.h"
-#include "elem/XRadial.h"
-#include "elem/XRamp.h"
-#include "elem/XRingGauge.h"
-#include "elem/XTogglebtn.h"
-
-// Ensure optional features are enabled in the configuration
+#include "XRadial.h"
+#include "XRamp.h"
+#include "XRingGauge.h"
+#include "XTogglebtn.h"
 //<Includes !End!>
 
 // ------------------------------------------------
@@ -40,6 +37,7 @@
 #endif
 #include <Adafruit_GFX.h>
 #include "Fonts/FreeSansBold12pt7b.h"
+#include "Fonts/FreeSansBold18pt7b.h"
 #include "Fonts/FreeSansBold9pt7b.h"
 //<Fonts !End!>
 
@@ -53,15 +51,14 @@
 // Enumerations for pages, elements, fonts, images
 // ------------------------------------------------
 //<Enum !Start!>
-enum {E_PG1,E_PG2,E_PG3,E_POP_KEYPAD_NUM};
+enum {E_PG1,E_PG2,E_PG3};
 enum {E_ELEM_BOX1,E_ELEM_BOX2,E_ELEM_BOX3,E_ELEM_BOX4,E_ELEM_BOX5
       ,E_ELEM_BOX6,E_ELEM_BOX7,E_ELEM_BTN10,E_ELEM_BTN5,E_ELEM_BTN6
-      ,E_ELEM_BTN7,E_ELEM_BTN8,E_ELEM_BTN9,E_ELEM_NUMINPUT1
-      ,E_ELEM_NUMINPUT2,E_ELEM_RADIALGAUGE1,E_ELEM_RAMPGAUGE3
-      ,E_ELEM_RINGGAUGE1,E_ELEM_TEXT1,E_ELEM_TEXT2,E_ELEM_TEXT3
-      ,E_ELEM_TOGGLE1,E_ELEM_KEYPAD_NUM};
+      ,E_ELEM_BTN7,E_ELEM_BTN8,E_ELEM_BTN9,E_ELEM_RADIALGAUGE1
+      ,E_ELEM_RAMPGAUGE3,E_ELEM_RINGGAUGE1,E_ELEM_TEXT1,E_ELEM_TEXT2
+      ,E_ELEM_TEXT3,E_ELEM_TEXT7,E_ELEM_TEXT8,E_ELEM_TOGGLE1};
 // Must use separate enum for fonts with MAX_FONT at end to use gslc_FontSet.
-enum {E_BUILTIN15X24,E_BUILTIN5X8,E_FREESANSBOLD12,E_FREESANSBOLD9
+enum {E_BUILTIN5X8,E_FREESANSBOLD12,E_FREESANSBOLD18,E_FREESANSBOLD9
       ,MAX_FONT};
 //<Enum !End!>
 
@@ -73,7 +70,7 @@ enum {E_BUILTIN15X24,E_BUILTIN5X8,E_FREESANSBOLD12,E_FREESANSBOLD9
 // Define the maximum number of elements and pages
 // ------------------------------------------------
 //<ElementDefines !Start!>
-#define MAX_PAGE                4
+#define MAX_PAGE                3
 
 #define MAX_ELEM_PG1 7 // # Elems total on page
 #define MAX_ELEM_PG1_RAM MAX_ELEM_PG1 // # Elems in RAM
@@ -88,10 +85,10 @@ enum {E_BUILTIN15X24,E_BUILTIN5X8,E_FREESANSBOLD12,E_FREESANSBOLD9
 // ------------------------------------------------
 // Create element storage
 // ------------------------------------------------
-gslc_tsGui                      m_gui;
-gslc_tsDriver                   m_drv;
-gslc_tsFont                     m_asFont[MAX_FONT];
-gslc_tsPage                     m_asPage[MAX_PAGE];
+gslc_tsGui m_gui;
+gslc_tsDriver m_drv;
+gslc_tsFont m_asFont[MAX_FONT];
+gslc_tsPage m_asPage[MAX_PAGE];
 
 //<GUI_Extra_Elements !Start!>
 gslc_tsElem                     m_asPage1Elem[MAX_ELEM_PG1_RAM];
@@ -100,11 +97,8 @@ gslc_tsElem                     m_asPage2Elem[MAX_ELEM_PG2_RAM];
 gslc_tsElemRef                  m_asPage2ElemRef[MAX_ELEM_PG2];
 gslc_tsElem                     m_asPage3Elem[MAX_ELEM_PG3_RAM];
 gslc_tsElemRef                  m_asPage3ElemRef[MAX_ELEM_PG3];
-gslc_tsElem                     m_asKeypadNumElem[1];
-gslc_tsElemRef                  m_asKeypadNumElemRef[1];
-gslc_tsXKeyPad                  m_sKeyPadNum;
-gslc_tsXTogglebtn               m_asXToggle1;
 gslc_tsXRingGauge               m_sXRingGauge1;
+gslc_tsXTogglebtn               m_asXToggle1;
 gslc_tsXRadial                  m_sXRadialGauge1;
 gslc_tsXRamp                    m_sXRampGauge3;
 
@@ -118,13 +112,12 @@ gslc_tsXRamp                    m_sXRampGauge3;
 
 // Element References for direct access
 //<Extern_References !Start!>
+extern gslc_tsElemRef* m_pElemCounter1;
+extern gslc_tsElemRef* m_pElemCounter2;
 extern gslc_tsElemRef* m_pElemRadial1;
 extern gslc_tsElemRef* m_pElemRamp2_3;
 extern gslc_tsElemRef* m_pElemToggle1;
-extern gslc_tsElemRef* m_pElemVal1;
-extern gslc_tsElemRef* m_pElemVal1_2;
 extern gslc_tsElemRef* m_pElemXRingGauge1;
-extern gslc_tsElemRef* m_pElemKeyPadNum;
 //<Extern_References !End!>
 
 // Define debug message function
@@ -133,39 +126,41 @@ static int16_t DebugOut(char ch);
 // ------------------------------------------------
 // Callback Methods
 // ------------------------------------------------
-bool CbBtnCommon(void* pvGui,void *pvElemRef,gslc_teTouch eTouch,int16_t nX,int16_t nY);
-bool CbCheckbox(void* pvGui, void* pvElemRef, int16_t nSelId, bool bState);
-bool CbDrawScanner(void* pvGui,void* pvElemRef,gslc_teRedrawType eRedraw);
-bool CbKeypad(void* pvGui, void *pvElemRef, int16_t nState, void* pvData);
-bool CbListbox(void* pvGui, void* pvElemRef, int16_t nSelId);
-bool CbSlidePos(void* pvGui,void* pvElemRef,int16_t nPos);
-bool CbSpinner(void* pvGui, void *pvElemRef, int16_t nState, void* pvData);
-bool CbTickScanner(void* pvGui,void* pvScope);
+bool CbBtnCommon(void *pvGui, void *pvElemRef, gslc_teTouch eTouch, int16_t nX, int16_t nY);
+bool CbCheckbox(void *pvGui, void *pvElemRef, int16_t nSelId, bool bState);
+bool CbDrawScanner(void *pvGui, void *pvElemRef, gslc_teRedrawType eRedraw);
+bool CbKeypad(void *pvGui, void *pvElemRef, int16_t nState, void *pvData);
+bool CbListbox(void *pvGui, void *pvElemRef, int16_t nSelId);
+bool CbSlidePos(void *pvGui, void *pvElemRef, int16_t nPos);
+bool CbSpinner(void *pvGui, void *pvElemRef, int16_t nState, void *pvData);
+bool CbTickScanner(void *pvGui, void *pvScope);
 
 // ------------------------------------------------
 // Create page elements
 // ------------------------------------------------
 void InitGUIslice_gen()
 {
-  gslc_tsElemRef* pElemRef = NULL;
+  gslc_tsElemRef *pElemRef = NULL;
 
-  if (!gslc_Init(&m_gui,&m_drv,m_asPage,MAX_PAGE,m_asFont,MAX_FONT)) { return; }
+  if (!gslc_Init(&m_gui, &m_drv, m_asPage, MAX_PAGE, m_asFont, MAX_FONT))
+  {
+    return;
+  }
 
   // ------------------------------------------------
   // Load Fonts
   // ------------------------------------------------
-//<Load_Fonts !Start!>
-    if (!gslc_FontSet(&m_gui,E_BUILTIN15X24,GSLC_FONTREF_PTR,NULL,3)) { return; }
+  //<Load_Fonts !Start!>
     if (!gslc_FontSet(&m_gui,E_BUILTIN5X8,GSLC_FONTREF_PTR,NULL,1)) { return; }
     if (!gslc_FontSet(&m_gui,E_FREESANSBOLD12,GSLC_FONTREF_PTR,&FreeSansBold12pt7b,1)) { return; }
+    if (!gslc_FontSet(&m_gui,E_FREESANSBOLD18,GSLC_FONTREF_PTR,&FreeSansBold18pt7b,1)) { return; }
     if (!gslc_FontSet(&m_gui,E_FREESANSBOLD9,GSLC_FONTREF_PTR,&FreeSansBold9pt7b,1)) { return; }
-//<Load_Fonts !End!>
+  //<Load_Fonts !End!>
 
-//<InitGUI !Start!>
+  //<InitGUI !Start!>
   gslc_PageAdd(&m_gui,E_PG1,m_asPage1Elem,MAX_ELEM_PG1_RAM,m_asPage1ElemRef,MAX_ELEM_PG1);
   gslc_PageAdd(&m_gui,E_PG2,m_asPage2Elem,MAX_ELEM_PG2_RAM,m_asPage2ElemRef,MAX_ELEM_PG2);
   gslc_PageAdd(&m_gui,E_PG3,m_asPage3Elem,MAX_ELEM_PG3_RAM,m_asPage3ElemRef,MAX_ELEM_PG3);
-  gslc_PageAdd(&m_gui,E_POP_KEYPAD_NUM,m_asKeypadNumElem,1,m_asKeypadNumElemRef,1);  // KeyPad
 
   // NOTE: The current page defaults to the first page added. Here we explicitly
   //       ensure that the main page is the correct page no matter the add order.
@@ -177,12 +172,6 @@ void InitGUIslice_gen()
   // -----------------------------------
   // PAGE: E_PG1
   
-  
-  // Create toggle button E_ELEM_TOGGLE1
-  pElemRef = gslc_ElemXTogglebtnCreate(&m_gui,E_ELEM_TOGGLE1,E_PG1,&m_asXToggle1,
-    (gslc_tsRect){350,220,100,50},GSLC_COL_GRAY,GSLC_COL_BLUE_DK1,GSLC_COL_GRAY_LT3,
-    true,false,&CbBtnCommon);
-  m_pElemToggle1 = pElemRef;
 
   // Create ring gauge E_ELEM_RINGGAUGE1 
   static char m_sRingText1[11] = "";
@@ -192,13 +181,6 @@ void InitGUIslice_gen()
   gslc_ElemXRingGaugeSetValRange(&m_gui, pElemRef, 0, 100);
   gslc_ElemXRingGaugeSetVal(&m_gui, pElemRef, 0); // Set initial value
   m_pElemXRingGauge1 = pElemRef;
-
-  // Create progress bar E_ELEM_RADIALGAUGE1 
-  pElemRef = gslc_ElemXRadialCreate(&m_gui,E_ELEM_RADIALGAUGE1,E_PG1,&m_sXRadialGauge1,
-    (gslc_tsRect){360,100,80,80},0,100,0,GSLC_COL_GREEN);
-  gslc_ElemXRadialSetIndicator(&m_gui,pElemRef,GSLC_COL_GREEN,20,3,false);
-  gslc_ElemXRadialSetTicks(&m_gui,pElemRef,GSLC_COL_GRAY,8,5);
-  m_pElemRadial1 = pElemRef;
   
   // create E_ELEM_BTN9 button with text label
   pElemRef = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN9,E_PG1,
@@ -224,16 +206,27 @@ void InitGUIslice_gen()
   gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
   gslc_ElemSetCol(&m_gui,pElemRef,((gslc_tsColor){75,120,205}),((gslc_tsColor){25,70,153}),((gslc_tsColor){75,120,205}));
   
-  // Create E_ELEM_NUMINPUT2 numeric input field
-  static char m_sInputNumber2[11] = "100";
-  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_NUMINPUT2,E_PG1,(gslc_tsRect){304,340,191,26},
-    (char*)m_sInputNumber2,11,E_BUILTIN15X24);
+  // Create toggle button E_ELEM_TOGGLE1
+  pElemRef = gslc_ElemXTogglebtnCreate(&m_gui,E_ELEM_TOGGLE1,E_PG1,&m_asXToggle1,
+    (gslc_tsRect){680,410,100,50},GSLC_COL_GRAY,GSLC_COL_BLUE_DK1,GSLC_COL_GRAY_LT3,
+    true,false,&CbBtnCommon);
+  m_pElemToggle1 = pElemRef;
+
+  // Create progress bar E_ELEM_RADIALGAUGE1 
+  pElemRef = gslc_ElemXRadialCreate(&m_gui,E_ELEM_RADIALGAUGE1,E_PG1,&m_sXRadialGauge1,
+    (gslc_tsRect){360,230,80,80},0,100,0,GSLC_COL_GREEN);
+  gslc_ElemXRadialSetIndicator(&m_gui,pElemRef,GSLC_COL_GREEN,20,3,false);
+  gslc_ElemXRadialSetTicks(&m_gui,pElemRef,GSLC_COL_GRAY,8,5);
+  m_pElemRadial1 = pElemRef;
+  
+  // Create E_ELEM_TEXT7 runtime modifiable text
+  static char m_sDisplayText7[5] = "100%";
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT7,E_PG1,(gslc_tsRect){710,5,85,28},
+    (char*)m_sDisplayText7,5,E_FREESANSBOLD18);
   gslc_ElemSetTxtAlign(&m_gui,pElemRef,GSLC_ALIGN_MID_MID);
-  gslc_ElemSetTxtMargin(&m_gui,pElemRef,5);
-  gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
-  gslc_ElemSetClickEn(&m_gui, pElemRef, true);
-  gslc_ElemSetTouchFunc(&m_gui, pElemRef, &CbBtnCommon);
-  m_pElemVal1_2 = pElemRef;
+  gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_GRAY,((gslc_tsColor){25,70,153}),((gslc_tsColor){25,70,153}));
+  gslc_ElemSetTxtEnc(&m_gui,pElemRef,GSLC_TXT_ENC_UTF8);
+  m_pElemCounter1 = pElemRef;
 
   // -----------------------------------
   // PAGE: E_PG2
@@ -322,36 +315,23 @@ void InitGUIslice_gen()
 
   // Create progress bar E_ELEM_RAMPGAUGE3 
   pElemRef = gslc_ElemXRampCreate(&m_gui,E_ELEM_RAMPGAUGE3,E_PG3,&m_sXRampGauge3,
-    (gslc_tsRect){300,170,200,80},0,75,
-    75,GSLC_COL_YELLOW,false);
+    (gslc_tsRect){300,170,200,80},0,100,
+    100,GSLC_COL_YELLOW,false);
+  gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_WHITE,((gslc_tsColor){25,70,153}),GSLC_COL_BLACK);
   m_pElemRamp2_3 = pElemRef;
   
-  // Create E_ELEM_NUMINPUT1 numeric input field
-  static char m_sInputNumber1[11] = "100";
-  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_NUMINPUT1,E_PG3,(gslc_tsRect){304,270,191,26},
-    (char*)m_sInputNumber1,11,E_BUILTIN15X24);
+  // Create E_ELEM_TEXT8 runtime modifiable text
+  static char m_sDisplayText8[5] = "100%";
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT8,E_PG3,(gslc_tsRect){710,5,83,28},
+    (char*)m_sDisplayText8,5,E_FREESANSBOLD18);
   gslc_ElemSetTxtAlign(&m_gui,pElemRef,GSLC_ALIGN_MID_MID);
-  gslc_ElemSetTxtMargin(&m_gui,pElemRef,5);
-  gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
-  gslc_ElemSetClickEn(&m_gui, pElemRef, true);
-  gslc_ElemSetTouchFunc(&m_gui, pElemRef, &CbBtnCommon);
-  m_pElemVal1 = pElemRef;
+  gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_GRAY,((gslc_tsColor){25,70,153}),((gslc_tsColor){25,70,153}));
+  gslc_ElemSetTxtEnc(&m_gui,pElemRef,GSLC_TXT_ENC_UTF8);
+  m_pElemCounter2 = pElemRef;
+  //<InitGUI !End!>
 
-  // -----------------------------------
-  // PAGE: E_POP_KEYPAD_NUM
-  
-  static gslc_tsXKeyPadCfg_Num sCfg;
-  sCfg = gslc_ElemXKeyPadCfgInit_Num();
-  gslc_ElemXKeyPadCfgSetFloatEn_Num(&sCfg, true);
-  gslc_ElemXKeyPadCfgSetSignEn_Num(&sCfg, true);
-  m_pElemKeyPadNum = gslc_ElemXKeyPadCreate_Num(&m_gui, E_ELEM_KEYPAD_NUM, E_POP_KEYPAD_NUM,
-    &m_sKeyPadNum, 65, 80, E_BUILTIN5X8, &sCfg);
-  gslc_ElemXKeyPadValSetCb(&m_gui, m_pElemKeyPadNum, &CbKeypad);
-//<InitGUI !End!>
-
-//<Startup !Start!>
-//<Startup !End!>
-
+  //<Startup !Start!>
+  //<Startup !End!>
 }
 
 #endif // end _GUISLICE_GEN_H
